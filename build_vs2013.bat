d6b1e5a454cbca96c4d4a3c5611a04b3190cd397
Bruno Barbieri <brunorex@gmail.com>
2014-10-07 21:35:15 -0300

    Delete old project files

--- build_vs2013.bat
+++ build_vs2013.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 CALL "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-msbuild /m /t:XviD4PSP_2010 /p:Configuration=Release /p:BuildProjectReferences=false XviD4PSP_2010.sln
+msbuild /m /t:XviD4PSP /p:Configuration=Release /p:BuildProjectReferences=false XviD4PSP.sln
 CD bin
 CALL "4Gb patcher.bat"
 PAUSE
--- clean_vs2013.bat
+++ clean_vs2013.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
 CALL "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
-msbuild /m /t:XviD4PSP_2010:clean /p:Configuration=Release /p:BuildProjectReferences=false XviD4PSP_2010.sln
+msbuild /m /t:XviD4PSP:clean /p:Configuration=Release /p:BuildProjectReferences=false XviD4PSP.sln
 PAUSE
